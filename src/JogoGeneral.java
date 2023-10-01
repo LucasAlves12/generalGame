7b288bf3cfd0cc109f073e83e40e76e571dca778
Lucas Alves <lucasantunesalves12@hotmail.com>
2023-10-01 12:27:02 -0300

    primeiras logicas dos dados

--- src/JogoGeneral.java
+++ src/JogoGeneral.java
@@ -25,11 +25,22 @@ public class JogoGeneral {
 
     public int validarJogada(int x){
         for(int i=0;i<5;i++){
-            cont[dados[i].getSideUp()-1] += 1;
+            cont[dados[i].getSideUp()-1]++;
         }
 
         switch(x){//fazer a logica de return direto de cada pontuação de cada tipo de jogada
             case 1://13 cases, 13 tipos de jogada
+                return cont[0]; //jogada de 1
+            case 2:
+                return cont[1]*2; //jogada de 2
+            case 3:
+                return cont[2]*3; //jogada de 3
+            case 4:
+                return cont[3]*4; //jogada de 4
+            case 5:
+                return cont[4]*5; //jogada de 5
+            case 6:    
+                return cont[5]*6; //jogada de 6
         }
 
 
